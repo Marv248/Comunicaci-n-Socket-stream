fde45349ae27f372c1056316b3ceb8ce6e50da37
cristo1208 <martinezchristopher518@gmail.com>
2026-01-20 08:00:34 -0600

    gerDoc

--- src/main/java/org/marco/chat/cliente/HistorialCliente.java
+++ src/main/java/org/marco/chat/cliente/HistorialCliente.java
@@ -1,5 +1,8 @@
 package org.marco.chat.cliente;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,8 +12,14 @@ public class HistorialCliente {
     public void agregarMensaje(String mensaje){
         mensajes.add(mensaje);
     }
-
-    public void guardarMensajes(){
-        // TODO: guardar los mensajes en un .txt
+    public void gerDoc(){
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("C:/Users/lebir/Downloads/conversacion.txt"))) {
+            for(int i = 0; i < mensajes.size(); i++){
+                bw.write(mensajes.get(i));
+                bw.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
